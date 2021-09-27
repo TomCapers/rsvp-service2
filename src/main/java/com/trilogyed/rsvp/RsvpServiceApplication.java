1e7179bd4aece93505d1c797c791248993eb3ce6
Tom Capers <tompcapers@gmail.com>
2021-09-27 14:05:35 -0700

    added a comment to test pipeline

--- src/main/java/com/trilogyed/rsvp/RsvpServiceApplication.java
+++ src/main/java/com/trilogyed/rsvp/RsvpServiceApplication.java
@@ -3,6 +3,7 @@ package com.trilogyed.rsvp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 @EnableCaching
@@ -11,5 +12,7 @@ public class RsvpServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RsvpServiceApplication.class, args);
 	}
+
+	//adding a comment to test pipeline
 
 }
